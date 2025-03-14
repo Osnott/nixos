a1d34c12481f56560e0128e9d4249e8b3b73d4f0
osnott <hi@osnott.xyz>
2025-03-14 09:17:50 +0000

    feat/fix(flake): fix home-manager not following current nixpkgs and add nvf as a flake input

--- flake.nix
+++ flake.nix
@@ -1,17 +1,18 @@
 {
-  description = "Your new nix config";
+  description = "osnott's nixos configuration!";
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # nvf
+    nvf.url = "github:notashelf/nvf";
+    nvf.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
@@ -72,7 +73,7 @@
         modules = [
           # > Our main home-manager configuration file <
           ./home/osnott/taffy.nix
-	  ./home/osnott/nixpkgs.nix
+          ./home/osnott/nixpkgs.nix
         ];
       };
     };
