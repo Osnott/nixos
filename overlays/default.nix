db136832ce442352acac981a64b9f5782e9605bd
osnott <hi@osnott.xyz>
2025-03-14 09:25:12 +0000

    fix(overlays): configure overlays properly

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,12 +12,7 @@
     # });
   };
 
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
-  unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
-      config.allowUnfree = true;
-    };
+  stable = final: _: {
+    stable = inputs.nixpkgs-stable.legacyPackages.${final.system};
   };
 }
