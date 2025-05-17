bda735a7e140eddb82f0feeac9b68dfe93213744
osnott <hi@osnott.xyz>
2025-05-17 02:44:30 +0000

    feat(taffy/flux): add atm9 modpack

--- hosts/common/optional/flux/default.nix
+++ hosts/common/optional/flux/default.nix
@@ -11,11 +11,44 @@
     enable = true;
 
     servers = {
-      prominence-2-he = {
-        package = pkgs.mkMinecraftServer {
-          name = "prominence-2-he";
-          src = ./servers/mc/prominence_2;
-          hash = "sha256-wBhrAIQgqZjc9AZQCdALyfvOjtV1ZM9jGRqJENgMS1o=";
+      # prominence-2-he = {
+      #   package = pkgs.mkMinecraftServer {
+      #     name = "prominence-2-he";
+      #     src = ./servers/mc/prominence_2;
+      #     hash = "sha256-wBhrAIQgqZjc9AZQCdALyfvOjtV1ZM9jGRqJENgMS1o=";
+      #   };
+      #
+      #   proxy = {
+      #     enable = true;
+      #
+      #     backend = "playit";
+      #   };
+      # };
+
+      atm9 = {
+        package = pkgs.mkGenericServer {
+          name = "atm9";
+
+          src = ./servers/mc/atm9-server-configured.tar.gz;
+
+          hash = "sha256-kN4DQpcMhmGg0HJfNU2xXExR/CpLJ7E98h+dvufBi14=";
+
+          nativeBuildInputs = [
+            pkgs.jre
+            pkgs.jre8
+          ];
+
+          buildInputs = [
+            pkgs.jre
+            pkgs.jre8
+          ];
+
+          buildPhase = ''
+            HOME=$TMPDIR
+            cp -r . $out
+          '';
+
+          startCmd = "./startserver.sh";
         };
 
         proxy = {
